a609df51947f74049b837d752dfe57db8224ec34
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-04-29 23:50:09 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { DetailsComponent } from './details/details.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { FilmsComponent } from './films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    DetailsComponent,
+    BlogComponent,
+    AboutComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
